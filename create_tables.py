4906286aeea8b5e098cbbce10f98df264e83d559
GrottI0 <chernega.bk@gmail.com>
2022-07-11 00:17:20 +0300

    create_tables exeption catch3

--- create_tables.py
+++ create_tables.py
@@ -11,8 +11,8 @@ load_dotenv()
 # функция создания таблиц базы данных
 def create_tables():
     engine = create_engine(os.environ['DB_URL'])
-    session = Session(bind=engine.connect())
     try:
+        session = Session(bind=engine.connect())
         session.execute('''CREATE TABLE users (
         id SERIAL NOT NULL PRIMARY KEY,
         full_name VARCHAR(256) NOT NULL,
@@ -20,18 +20,24 @@ def create_tables():
         password VARCHAR(256) NOT NULL,
         roles VARCHAR(256) NOT NULL,
         blocked BOOLEAN DEFAULT false);''')
+        session.commit()
+        session.close()
     except ProgrammingError:
         print('users already exists')
 
     try:
+        session = Session(bind=engine.connect())
         session.execute('''CREATE TABLE sections (
         id SERIAL NOT NULL PRIMARY KEY,
         name VARCHAR(256) NOT NULL,
         created_by_id INTEGER REFERENCES users (id) ON DELETE CASCADE);''')
+        session.commit()
+        session.close()
     except ProgrammingError:
         print('sections already exists')
 
     try:
+        session = Session(bind=engine.connect())
         session.execute('''CREATE TABLE articles (
         id SERIAL NOT NULL PRIMARY KEY,
         creator VARCHAR(256) NOT NULL,
@@ -46,34 +52,42 @@ def create_tables():
         tags VARCHAR(256) NOT NULL,
         approved_at VARCHAR(256),
         section_id INTEGER REFERENCES sections (id) ON DELETE CASCADE);''')
+        session.commit()
+        session.close()
     except ProgrammingError:
         print('articles already exists')
 
     try:
+        session = Session(bind=engine.connect())
         session.execute('''CREATE TABLE comments (
         id SERIAL NOT NULL PRIMARY KEY,
         user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
         article_id INTEGER REFERENCES articles (id) ON DELETE CASCADE,
         text TEXT NOT NULL);''')
+        session.commit()
+        session.close()
     except ProgrammingError:
         print('comments already exists')
 
     try:
+        session = Session(bind=engine.connect())
         session.execute('''CREATE TABLE ratings (
         id SERIAL NOT NULL PRIMARY KEY,
         user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
         article_id INTEGER REFERENCES articles (id) ON DELETE CASCADE,
         rating INTEGER NOT NULL);''')
+        session.commit()
+        session.close()
     except ProgrammingError:
         print('rating already exists')
 
     try:
+        session = Session(bind=engine.connect())
         session.execute('''CREATE TABLE sessions (
         id SERIAL NOT NULL PRIMARY KEY,
         user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
         session_id VARCHAR(256) NOT NULL UNIQUE);''')
+        session.commit()
+        session.close()
     except ProgrammingError:
         print('sessions already exists')
-
-    session.commit()
-    session.close()
