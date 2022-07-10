ebaee806ed7296d07e29e914f94ceb32c0930f08
GrottI0 <chernega.bk@gmail.com>
2022-07-10 23:47:00 +0300

    crete tables fix

--- create_tables.py
+++ create_tables.py
@@ -17,8 +17,8 @@ def create_tables():
     full_name VARCHAR(256) NOT NULL,
     email VARCHAR(256) NOT NULL UNIQUE,
     password VARCHAR(256) NOT NULL,
-    roles VARCHAR(256) NOT NULL),
-    blocked BOOLEAN DEFAULT false;''')
+    roles VARCHAR(256) NOT NULL,
+    blocked BOOLEAN DEFAULT false);''')
 
     session.execute('''CREATE TABLE sections (
     id SERIAL NOT NULL PRIMARY KEY,
